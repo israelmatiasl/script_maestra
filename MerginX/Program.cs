2dc057ba9a485bbfd772503ebd9a738b84d378db
Israel <israelmatiasl@gmail.com>
2019-09-20 11:17:18 -0500

    Corrección de rutas y validaciones

--- MerginX/Program.cs
+++ MerginX/Program.cs
@@ -20,16 +20,16 @@ namespace MerginX
             var fechaManiana = fechaActual.AddDays(1);
             var fechaManianaEntero = Functions.ConvertToDateString(fechaManiana);
 
-            var paths = leerRutas(@"./path/rutas.txt");
+            var paths = leerRutas("/mnt/fileszpatidesa/test/maestranetcore/app/script_maestra/MerginX/path/rutas.txt", fechaActual);
 
             //var pathDescuentosAgotados = $"/Users/israelmatiasl/PRINCIPAL/Proyectos/SFTP/CORRECCION_MAESTRA/0821/descuentos_agotados_{fechaActualEntero}.csv"; //$"/mnt/fileszpatidesa/test/maestranetcore/20190821/descuentos_agotados_{fechaActualEntero}.csv";// $"{paths.FirstOrDefault(x=>x.Name == "AGOTADOS").Value}/descuentos_agotados_{fechaActualEntero}.csv";
-            var pathDescuentosAgotados = $"{paths.FirstOrDefault(x => x.Name == "AGOTADOS").Value}/descuentos_agotados_{fechaActualEntero}.csv";
+            var pathDescuentosAgotados = $"{paths.FirstOrDefault(x => x.Name == "AGOTADOS").Value}";
             //var pathMaestraDescuentos = $"/Users/israelmatiasl/PRINCIPAL/Proyectos/SFTP/CORRECCION_MAESTRA/0821/Maestra_Descuentos_20190724.xlsx"; //$"/mnt/fileszpatidesa/test/maestranetcore/20190821/Maestra_Descuentos_{fechaActualEntero}.xlsx";// $"{paths.FirstOrDefault(x => x.Name == "MAESTRA").Value}/Maestra_Descuentos_{fechaActualEntero}.xlsx"; //$"/Users/israelmatiasl/PRINCIPAL/Proyectos/SFTP/CORRECCION_MAESTRA/0724/Maestra_Descuentos_{fechaActualEntero}.xlsx";
-            var pathMaestraDescuentos = $"{paths.FirstOrDefault(x => x.Name == "MAESTRA").Value}/Maestra_Descuentos_{fechaActualEntero}.xlsx";
+            var pathMaestraDescuentos = $"{paths.FirstOrDefault(x => x.Name == "MAESTRA").Value}";
             //var pathMaestraDescuentosSalida = $"/Users/israelmatiasl/PRINCIPAL/Proyectos/SFTP/CORRECCION_MAESTRA/0821/salida"; //$"/mnt/fileszpatidesa/test/maestranetcore/20190821";// $"{paths.FirstOrDefault(x => x.Name == "OUT").Value}";//"/Users/israelmatiasl/PRINCIPAL/Proyectos/SFTP/CORRECCION_MAESTRA/0724/salida";
             var pathMaestraDescuentosSalida = $"{paths.FirstOrDefault(x => x.Name == "OUT").Value}";
             //var pathNuevosDescuentosTSV = $"/Users/israelmatiasl/PRINCIPAL/Proyectos/SFTP/CORRECCION_MAESTRA/20190821/hive_output_{fechaActualEntero}.tsv"; //$"/mnt/fileszpatidesa/test/maestranetcore/20190821/hive_output_{fechaActualEntero}.tsv";// $"{paths.FirstOrDefault(x => x.Name == "NUEVOS").Value}/hive_output_{fechaActualEntero}.tsv";// $"/Users/israelmatiasl/PRINCIPAL/Proyectos/SFTP/190723/hive_output.tsv";
-            var pathNuevosDescuentosTSV = $"{paths.FirstOrDefault(x => x.Name == "NUEVOS").Value}/hive_output_{fechaActualEntero}.tsv";
+            var pathNuevosDescuentosTSV = $"{paths.FirstOrDefault(x => x.Name == "NUEVOS").Value}";
             //var pathNuevosDescuentosXLS = "/Users/israelmatiasl/PRINCIPAL/Proyectos/SFTP/CORRECCION_MAESTRA/0821/query-hive-2977.xlsx";
 
             modificarMaestra(pathMaestraDescuentos,
@@ -38,11 +38,9 @@ namespace MerginX
                              pathMaestraDescuentosSalida,
                              fechaActualEntero,
                              fechaManianaEntero);
-
-            Console.ReadLine();
         }
 
-        static List<ExcelPath> leerRutas(string path)
+        static List<ExcelPath> leerRutas(string path, DateTime fechaActual)
         {
             var paths = new List<ExcelPath>();
             string[] lines = File.ReadAllLines(path);
@@ -52,6 +50,60 @@ namespace MerginX
                 var dum = line.Split('|');
                 var pathVal = new ExcelPath(dum[0], dum[1]);
                 paths.Add(pathVal);
+            }
+
+            var pathDescuentosAgotados = paths.FirstOrDefault(x => x.Name == "AGOTADOS");
+            if (pathDescuentosAgotados != null)
+            {
+                var today = fechaActual;
+                while (true)
+                {
+                    var todayNumero = Functions.ConvertToDateString(today);
+                    var pathFinal = $"{pathDescuentosAgotados.Value}/descuentos_agotados_{todayNumero}.csv";
+                    if (!File.Exists(pathFinal))
+                    {
+                        today = today.AddDays(-1);
+                        continue;
+                    }
+                    pathDescuentosAgotados.Value = pathFinal;
+                    break;
+                }
+            }
+
+            var pathMaestra = paths.FirstOrDefault(x => x.Name == "MAESTRA");
+            if (pathMaestra != null)
+            {
+                var today = fechaActual;
+                while (true)
+                {
+                    var todayNumero = Functions.ConvertToDateString(today);
+                    var pathFinal = $"{pathMaestra.Value}/Maestra_Descuentos_{todayNumero}.xlsx";
+                    if (!File.Exists(pathFinal))
+                    {
+                        today = today.AddDays(-1);
+                        continue;
+                    }
+                    pathMaestra.Value = pathFinal;
+                    break;
+                }
+            }
+
+            var pathNuevos = paths.FirstOrDefault(x => x.Name == "NUEVOS");
+            if (pathNuevos != null)
+            {
+                var today = fechaActual;
+                while (true)
+                {
+                    var todayNumero = Functions.ConvertToDateString(today);
+                    var pathFinal = $"{pathNuevos.Value}/hive_output_{todayNumero}.tsv";
+                    if (!File.Exists(pathFinal))
+                    {
+                        today = today.AddDays(-1);
+                        continue;
+                    }
+                    pathNuevos.Value = pathFinal;
+                    break;
+                }
             }
 
             return paths;
@@ -69,47 +121,6 @@ namespace MerginX
             //var descuentosQueryNuevos = ExcelService.LeerQueryMaestra(pathDescuentosNuevos);
             Console.WriteLine("[{0}] Se terminó de extraer información de los descuentos Nuevos...", DateTime.Now);
 
-            //Console.ForegroundColor = ConsoleColor.Yellow;
-            //Console.WriteLine("Hay beneficios totales: {0}", descuentosQueryNuevos.Count);
-            //Console.ForegroundColor = ConsoleColor.White;
-            //
-            //Console.ForegroundColor = ConsoleColor.Green;
-            //var agrupadogrupobenef = descuentosQueryNuevos.Where(x => x.FlagEstado == "1")
-            //    .GroupBy(x=> new { x.CodGrupoDescuento, x.CodDescuento }).ToList();
-            //Console.WriteLine("Hay cod grupos y cod beneficios diferentes: {0}", agrupadogrupobenef.Count);
-            //
-            //foreach (var agrup in agrupadogrupobenef)
-            //{
-            //    if (agrup.Count() > 1)
-            //    {
-            //        foreach(var gru in agrup)
-            //        {
-            //            Console.ForegroundColor = ConsoleColor.Red;
-            //            Console.WriteLine("FUENTE {0} ID GRUPO: {1} - ID BENEFICIO {2} - SEGMENTO {3} - LATITUD:LONGITUD {4}:{5} - FECHAPROC: {6}",gru.DesCodFuenteDescuento, gru.CodGrupoDescuento, gru.CodDescuento, gru.SegmentoCliente, gru.Latitud, gru.Longitud, gru.FechaProceso);
-            //        }
-            //    }
-            //}
-            //
-            //var agrupado = descuentosQueryNuevos.Where(x => x.FlagEstado == "1").GroupBy(x => new { x.Latitud, x.Longitud });
-            //
-            //Console.WriteLine("Hay {0} localidades agrupadas", agrupado.Count());
-            //foreach(var localidades in agrupado)
-            //{
-            //    var totalPorLocalidad = localidades.Count();
-            //
-            //    var totalUnicos = localidades.Select(x=>x.CodDescuento).Distinct().Count();
-            //
-            //    if (totalPorLocalidad != totalUnicos)
-            //    {
-            //        Console.ForegroundColor = ConsoleColor.Cyan;
-            //        Console.WriteLine("Fallado");
-            //        Console.ForegroundColor = ConsoleColor.Red;
-            //        foreach(var loc in localidades)
-            //        {
-            //            Console.WriteLine("ID GRUPO: {0} - ID BENEFICIO {1} - DESC: {2}",loc.CodGrupoDescuento, loc.CodDescuento, loc.TituloDescuento);
-            //        }
-            //    }
-            //}
 
             Console.WriteLine("[{0}] Empezando a extraer información de la Maestra...", DateTime.Now);
             var maestraCompleta = ExcelService.LeerMaestraCompleta(pathMaestraExcel);
@@ -117,9 +128,6 @@ namespace MerginX
 
             var descuentosMaestra = maestraCompleta.MaestraDescuentos;
             Console.WriteLine("[{0}] La maestra tiene {1} registros", DateTime.Now, descuentosMaestra.Count);
-            //Corroboración
-            //var maestraUnicos = descuentosMaestra.Select(x => new { IDBENEFICIO = x.IdBeneficio, IDGRUPOBENEFICIO = x.IdGrupoBeneficio }).Distinct().ToList();
-            //Console.WriteLine("[{0}] Los descuentos con ID BENEFICIO ÚNICOS SON {1}", DateTime.Now, maestraUnicos.Count);
 
             // Obtengo los Id's de los descuentos agotados
             var descuentosAgotadosIdGrupoBeneficios = descuentosAgotados.Select(x => x.IdGrupoBeneficio).Distinct().ToList();
@@ -149,7 +157,7 @@ namespace MerginX
 
             // Exporta la maestra de descuentos modificado
             Console.WriteLine("..{0}", descuentosMaestra.Count);
-            maestraCompleta.MaestraDescuentos = descuentosMaestra.Where(x=>x.FlagEstado == "1").ToList();
+            maestraCompleta.MaestraDescuentos = descuentosMaestra.Where(x=>x.FlagEstado != "0").ToList();
             Console.WriteLine("[{0}] La nueva Maestra ahora tiene {1} datos", DateTime.Now, maestraCompleta.MaestraDescuentos.Count);
             Console.WriteLine("[{0}] Empezando a exportar la Maestra", DateTime.Now);
             ExcelService.ExportarMaestraModificada(maestraCompleta, pathMaestraSalida, fechaManianaEntero);
